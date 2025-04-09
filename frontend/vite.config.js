436be7957ee451ae6e10c962f4977eac3974593e
Arish <arish@gmail.com>
2025-04-09 11:59:52 +0530

    add static.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
 });
